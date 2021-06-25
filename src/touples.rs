989a0cfdb741840c688ee990f89c2695b4123960
Patrick Gebhardt <patrick.gebhardt@gmail.com>
2021-06-26 00:29:30 +0200

    added touples

--- src/touples.rs
+++ src/touples.rs
@@ -2,5 +2,6 @@
 // max 12 elements
 
 pub fn run() {
-
+    let person: (&str, &str, i8, String) = ("Brad", "Mass", 57, String::from("Wales"));
+    println!("{} is from {} and is {} Land: {}", person.0, person.1, person.2, person.3);
 }
